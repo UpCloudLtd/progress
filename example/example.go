e60236071c9139c7628dd6cc9495cd99275eeeff
Toni Kangas <toni.kangas@upcloud.com>
2026-06-11 12:13:41 +0300

    refactor(example): use consts for message keys

--- example/example.go
+++ example/example.go
@@ -11,13 +11,22 @@ import (
 
 const loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
+const (
+	firstKey    = "first-example"
+	parallelKey = "parallel-example"
+	progressKey = "progress-example"
+	unknownKey  = "unknown-example"
+	pendingKey  = "pending-example"
+	longKey     = "long-example"
+)
+
 func main() {
 	taskLog := progress.NewProgress(nil)
 	taskLog.Start()
 	defer taskLog.Stop()
 
 	_ = taskLog.Push(messages.Update{
-		Key:     "first-example",
+		Key:     firstKey,
 		Message: "Progress is a library for communicating CLI app progress to the user",
 		Status:  messages.MessageStatusStarted,
 	})
@@ -25,13 +34,13 @@ func main() {
 	time.Sleep(time.Millisecond * 1500)
 
 	_ = taskLog.Push(messages.Update{
-		Key:     "parallel-example",
+		Key:     parallelKey,
 		Message: "There can be multiple active progress messages at once",
 		Status:  messages.MessageStatusStarted,
 	})
 
 	_ = taskLog.Push(messages.Update{
-		Key:     "progress-example",
+		Key:     progressKey,
 		Message: "Progress message can include part that is only outputted to TTY terminals",
 		Status:  messages.MessageStatusStarted,
 	})
@@ -39,19 +48,19 @@ func main() {
 	time.Sleep(time.Millisecond * 300)
 	for i := 1; i < 10; i++ {
 		_ = taskLog.Push(messages.Update{
-			Key:             "progress-example",
+			Key:             progressKey,
 			ProgressMessage: fmt.Sprintf("(%d%%)", i*10),
 		})
 		time.Sleep(time.Millisecond * 300)
 	}
 
 	_ = taskLog.Push(messages.Update{
-		Key:    "progress-example",
+		Key:    progressKey,
 		Status: messages.MessageStatusSuccess,
 	})
 
 	_ = taskLog.Push(messages.Update{
-		Key:     "first-example",
+		Key:     firstKey,
 		Message: "Progress messages can be updated while they are in pending or started state",
 		Status:  messages.MessageStatusStarted,
 	})
@@ -59,7 +68,7 @@ func main() {
 	time.Sleep(time.Millisecond * 1500)
 
 	_ = taskLog.Push(messages.Update{
-		Key:     "parallel-example",
+		Key:     parallelKey,
 		Status:  messages.MessageStatusError,
 		Details: "Error: Message details can be used, for example, to communicate error messages to the user.",
 	})
@@ -67,13 +76,13 @@ func main() {
 	time.Sleep(time.Millisecond * 1500)
 
 	_ = taskLog.Push(messages.Update{
-		Key:     "unknown-example",
+		Key:     unknownKey,
 		Status:  messages.MessageStatusStarted,
 		Message: "If message has started status when log is closed, its status is set to unknown",
 	})
 
 	_ = taskLog.Push(messages.Update{
-		Key:     "pending-example",
+		Key:     pendingKey,
 		Status:  messages.MessageStatusPending,
 		Message: "Pending tasks are not written to output. If message has pending status when log is closed, its status is set to skipped",
 	})
@@ -81,7 +90,7 @@ func main() {
 	time.Sleep(time.Millisecond * 1500)
 
 	_ = taskLog.Push(messages.Update{
-		Key:     "first-example",
+		Key:     firstKey,
 		Message: "Progress messages are, by default, written to stderr",
 		Status:  messages.MessageStatusSuccess,
 	})
@@ -89,7 +98,7 @@ func main() {
 	time.Sleep(time.Millisecond * 1500)
 
 	_ = taskLog.Push(messages.Update{
-		Key:     "long",
+		Key:     longKey,
 		Message: "Long messages are truncated - " + loremIpsum,
 		Details: loremIpsum,
 		Status:  messages.MessageStatusStarted,
@@ -98,7 +107,7 @@ func main() {
 	time.Sleep(time.Second * 3)
 
 	_ = taskLog.Push(messages.Update{
-		Key:    "long",
+		Key:    longKey,
 		Status: messages.MessageStatusWarning,
 	})
 
